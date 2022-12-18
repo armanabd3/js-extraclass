ffa9acd58ec6e0271e23d35e050dbc4ca7a659e4
armanabd3 <114800124+armanabd3@users.noreply.github.com>
2022-12-18 09:40:54 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function showAlert() {
+    alert('arman')
+}
+
+document.getElementById('clickbtn').addEventListener('click',showAlert)
+
+var test = 'arman'
+
+
+document.write(test.toUpperCase())
